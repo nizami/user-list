b109a9ea666cb02a52169e2f7c9ddf14da68f207
nizami <nizami.dev@gmail.com>
2024-06-19 16:39:32 +0300

    Update users list header styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UsersListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'user-list';
--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { UserCardItemComponent } from './user-card-item/user-card-item.component';
+import { UserListItemComponent } from './user-list-item/user-list-item.component';
 
 @Component({
   selector: 'app-users-list',
   standalone: true,
-  imports: [],
+  imports: [UserListItemComponent, UserCardItemComponent],
   templateUrl: './users-list.component.html',
-  styleUrl: './users-list.component.scss'
+  styleUrl: './users-list.component.scss',
 })
-export class UsersListComponent {
-
-}
+export class UsersListComponent {}
